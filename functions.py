39a61b7b90ee90ac8ca6f8ee8a6e63bce3e000fe
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-12 00:19:55 +0100

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+import numpy as np
+import streamlit as st
+import keras
+from keras import layers, models, optimizers  # modeling
+from PIL import Image
+
+MODEL = "pneumonia_detection_cnn.h5"
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    print("loading model")
+    model = keras.models.load_model(f"{MODEL}", compile=True)
+    return model
+
+def preprocess_image(img):
+    image = Image.open(img).convert("RGB")
+    p_img = image.resize((150, 150))
+    return np.array(p_img) / 255
+
+def predict(model, img):
+    prob = model.predict(np.reshape(img, [-1, 150, 150, 1]))[0]
+    if prob >= 0.5:
+        prediction = True
+    else:
+        prediction = False
+    return prob, prediction
