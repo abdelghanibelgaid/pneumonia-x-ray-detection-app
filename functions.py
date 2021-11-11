21275e1cfa4563953f6f7142e85e247dadc4af9d
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-11 21:29:37 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -21,7 +21,7 @@ def preprocess_image(img):
     return np.array(p_img) / 255.0
 
 def predict(model, img):
-    prob = model.predict(np.reshape(img, [150, 150]))
+    prob = model.predict(np.reshape(img, [-1, 150, 150, 1]))
     if prob > 0.5:
         prediction = True
     else:
