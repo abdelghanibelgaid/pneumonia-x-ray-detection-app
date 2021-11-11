68cb6523c4fcba97c96492a56da3e39a98a46e90
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-11 21:31:05 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,20 +9,17 @@ from PIL import Image
 MODEL = "pneumonia_detection_cnn.h5"
 img_size = 150
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    print("loading model")
-    model = load_model('pneumonia_detection_cnn.h5', compile=True)
-    return model
-
-def preprocess_image(img):
-    image = Image.open(img).convert("RGB")
-    p_img = image.resize((150, 150))
-    return np.array(p_img) / 255.0
-
-def predict(model, img):
-    prob = model.predict(np.reshape(img, [-1, 150, 150, 1]))
-    if prob > 0.5:
+def predict(image1): 
+    image = load_img(image1, target_size=(150, 150))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    #image = preprocess_input(image)
+    # predict the probability across all output classes
+    yhat = model.predict(image)
+    if yhat > 0.5:
         prediction = True
     else:
         prediction = False
