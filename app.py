02a005fcf4ee7cce9936ca135e5a4434e59edc63
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-23 22:35:22 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+def main():
+    menu_list = ['About', 'Pneumonia Detection','Breast Cancer Detection', 'Brain Tumor Detection']
+    
+    # Display the Sidebar
+    st.sidebar.title('Navigation')
+    menu_selection = st.sidebar.radio('', menu_list, index=2, key=None)
+    
+    # Display text in Sidebar
+    about.display_sidebar()
+    
+    # Selecting About Menu
+    if menu_selection == 'About':
+        about.display_about()
+    
+    # Selectiong Pneumonia Detection Web App
+    if menu_selection == 'Pneumonia Detection':
+        pneumonia.display_webapp_1()
+    
+    # Selectiong Breast Cancer Detection Web App
+    if menu_selection == 'Breast Cancer Detection':
+        about.display_about()
+    
+    # Selectiong Brain Tumor Detection Web App
+    if menu_selection == 'Brain Tumor Detection':
+        about.display_about()
+
+if __name__ == '__main__':
+    main()
