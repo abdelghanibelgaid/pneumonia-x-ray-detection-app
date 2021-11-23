8c055b97ad2cffbb52bc2f520adbb8975c94e49e
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-23 23:12:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,27 @@ import numpy as np
 from PIL import Image
 import about
 import functions
-def main():
-    menu_list = ['About', 'Pneumonia Detection','Breast Cancer Detection', 'Brain Tumor Detection']
+menu_list = ['About', 'Pneumonia Detection','Breast Cancer Detection', 'Brain Tumor Detection']
     
-    # Display the Sidebar
-    st.sidebar.title('Navigation')
-    menu_selection = st.sidebar.radio('', menu_list, index=2, key=None)
+# Display the Sidebar
+st.sidebar.title('Navigation')
+menu_selection = st.sidebar.radio('', menu_list, index=2, key=None)
     
-    # Display text in Sidebar
-    about.display_sidebar()
+# Display text in Sidebar
+about.display_sidebar()
     
-    # Selecting About Menu
-    if menu_selection == 'About':
-        about.display_about()
+# Selecting About Menu
+if menu_selection == 'About':
+    about.display_about()
     
-    # Selectiong Pneumonia Detection Web App
-    if menu_selection == 'Pneumonia Detection':
-        display_webapp_1()
+# Selectiong Pneumonia Detection Web App
+if menu_selection == 'Pneumonia Detection':
+    display_webapp_1()
     
-    # Selectiong Breast Cancer Detection Web App
-    if menu_selection == 'Breast Cancer Detection':
-        about.display_about()
+# Selectiong Breast Cancer Detection Web App
+if menu_selection == 'Breast Cancer Detection':
+    about.display_about()
     
-    # Selectiong Brain Tumor Detection Web App
-    if menu_selection == 'Brain Tumor Detection':
-        about.display_about()
-
-if __name__ == '__main__':
-    main()
+# Selectiong Brain Tumor Detection Web App
+if menu_selection == 'Brain Tumor Detection':
+    about.display_about()
