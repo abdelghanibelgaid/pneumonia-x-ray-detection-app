8e6d69d7e85176e6a8d2d01eafd8022d62c13ea4
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-27 03:19:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if export_as_pdf:
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, 'Pantient Name:' report_text)
+    pdf.cell(40, 10, 'Pantient Name:'+report_text)
     
     html = create_download_link(pdf.output(dest="S").encode("latin-1"), pdf_name)
 
