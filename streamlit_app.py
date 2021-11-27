4c549f9a34ef263330498d2f473e6cda50063b91
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-27 03:09:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if img is not None:
 from fpdf import FPDF
 import base64
 
-st.markdown('# Report Generator')
+st.markdown('### Report Generator')
 report_text = st.text_input("Enter the Name of the patient")
 report_text = st.text_input("Enter the ID of the patient")
 
@@ -60,7 +60,7 @@ export_as_pdf = st.button("Export Report")
 
 def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download Report</a>'
 
 if export_as_pdf:
     pdf = FPDF()
@@ -68,6 +68,6 @@ if export_as_pdf:
     pdf.set_font('Arial', 'B', 16)
     pdf.cell(40, 10, report_text)
     
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+    html = create_download_link(pdf.output(dest="S").encode("latin-1"), report_text)
 
     st.markdown(html, unsafe_allow_html=True)
