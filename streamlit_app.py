fa093391044f34c533e5ebdacbda4c3d83fee568
Abdelghani Belgaid <72890326+abdelghanibelgaid@users.noreply.github.com>
2021-11-27 03:10:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,6 @@ if export_as_pdf:
     pdf.set_font('Arial', 'B', 16)
     pdf.cell(40, 10, report_text)
     
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), report_text)
+    html = create_download_link(pdf.output(dest="S").encode("latin-1"), report_text+str(_pneumonia_diagnosis)
 
     st.markdown(html, unsafe_allow_html=True)
